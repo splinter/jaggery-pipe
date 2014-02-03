5aff41f42435a20460ef3ee74d733102a0d014d1
sameeram <mgjaye@gmail.com>
2014-02-03 19:03:56 +0530

    Removed plugins subfolders

--- plugins/basicAuth/basicAuth.js
+++
@@ -1,9 +0,0 @@
-var name = 'defaultBasicAuth';
-
-var handle = function (req, res, session, handlers) {
-    var log = new Log();
-    log.info('Basic authentication handler called!');
-    //handlers({code:403,msg:'Need to login'});
-    res.addHeader('WWW-Authenticate','Basic realm="restricted"');
-    res.sendError(401);
-};
